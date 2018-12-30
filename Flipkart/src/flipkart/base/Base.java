c36627f34922c592678a1b58993f0f4077eb9431
Jitinder Pal <Jitinder Pal@DESKTOP-U8EHGOG>
2018-12-30 15:27:44 +0530

    new base

--- Flipkart/src/flipkart/base/Base.java
+++ Flipkart/src/flipkart/base/Base.java
@@ -1,32 +1,69 @@
 package flipkart.base;
 
-import java.util.ArrayList;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
 import flipkart.OR.OR;
 import flipkart.Pages.HomePage;
+import flipkart.Pages.LoginPage;
 import flipkart.constants.Constants;
 
-public class Base implements OR {
+public class Base {
 	
-	public static WebDriver driver;
-	public Base base;
+	public WebDriver driver;
+//	public Base base;
 	public HomePage homePage; 
-	private WebDriverWait wait;
-	Logger log=Logger.getLogger(Base.class);
+	public LoginPage lp;
+	public WebDriverWait wait;
+	private static Map<String, BasePage> objRep = new HashMap<String, BasePage>();
+	private Wait<WebDriver> w;
+//	public Properties prop;
+	
+	
+	public static BasePage getInstance(String input)
 	{
-		wait = new WebDriverWait(driver, 60);
-		base = new Base();
+		BasePage interfaceType =null;
+		if(objRep.containsKey(input))
+		{
+			return (BasePage) objRep.get(input);
+		}
+		try {
+			   String className = "flipkart.Pages."+input;// really passed in from config
+			   Class c = Class.forName(className);
+			   interfaceType = (BasePage)c.newInstance();
+			   objRep.put(input, interfaceType);
+//			   if (className.equals("com.path.to.ImplementationType") {
+//			      ((ImplementationType)interfaceType).doSomethingOnlyICanDo();
+//			   } 
+			} catch (Exception e) {
+			   e.printStackTrace();
+			}
+		return interfaceType;
+	}
+	
+	@BeforeSuite
+	public void befSuite() 
+	{
+		System.out.println("befs runing");
+//		wait = new WebDriverWait(driver, 60);
+//		homePage = new HomePage();
+//		lp = new LoginPage();
 	}
 	
 	@BeforeClass
@@ -39,16 +76,16 @@ public class Base implements OR {
 		{
 			driver = new ChromeDriver();
 			System.out.println("chrome opened");
-			log.debug("Chrome Opened");
+			//log.debug("Chrome Opened");
 		}
 		else if (Constants.Browser.equalsIgnoreCase("firefox"))
 		{
 			driver = new FirefoxDriver();
 		}
 		
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+//		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.navigate().to(Constants.URL);
+		driver.navigate().to("https://www.flipkart.com");
 		
 		
 		}
@@ -59,11 +96,26 @@ public class Base implements OR {
 		}
 	}
 	
+//	public String getProperty(String key)
+//	{
+//		String propName = prop.getProperty(key);
+//		return propName;
+//	}
+	
+	
 	public WebElement findElement(By by)
-	{
+	{		
+		waitUntilPresence(by);
 		
-	return driver.findElement(by); 
+	return driver.findElement(by);
 	}
+	
+//	public void test()
+//	{
+//		WebElement e =driver.findElement(By.cssSelector("font-weight"));
+//		e.size
+//		
+//	}
 	
 	public  void type(By locator,String input)
 	{
@@ -83,24 +135,29 @@ public class Base implements OR {
 	}
 	
 	public void elementTobeVisible(By locator)
-	{
-		
-		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
-		WebElement element=driver.findElement(locator);
-		wait.until(ExpectedConditions.visibilityOf(element));
-		
+	{		
 		if(locator.toString().contains("link"))
 		{
-			wait.until(ExpectedConditions.elementToBeClickable(element));
+			wait.until(ExpectedConditions.elementToBeClickable(findElement(locator)));
+			System.out.println("2");
 		}
+		else if(locator.toString().toLowerCase().contains("chk"))
+		{
+			wait.until(ExpectedConditions.elementToBeSelected(locator));
+		}	
+	}
+	
+	public boolean isElementVisible(By locator)
+	{
+		return driver.findElement(locator).isDisplayed() ? true:false;
 		
 	}
 	
-	public static ArrayList<WebElement> findElements(By locator)
-	{
-		ArrayList<WebElement> list= (ArrayList<WebElement>) driver.findElements(locator);
-		return list;
-	}
+//	public static ArrayList<WebElement> findElements(By locator)
+//	{
+//		ArrayList<WebElement> list= (ArrayList<WebElement>) driver.findElements(locator);
+//		return list;
+//	}
 	
 	public String get_Attribute(By locator, String attribute)
 	{
@@ -110,9 +167,62 @@ public class Base implements OR {
 	public void click_Menu(String Menu, String Submenu) throws InterruptedException
 	{
 //		elementTobeVisible(By.xpath("//span[text()='"+Menu+"']"));
-		Thread.sleep(1000);  
+//		Thread.sleep(1000);  
 		driver.findElement(By.xpath("//span[text()='"+Menu+"']")).click();
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//a[text()='"+Submenu+"']")).click();
 	}
+	
+	public void click_Menu2(String Menu, String Submenu) throws InterruptedException
+	{
+//		elementTobeVisible(By.xpath("//span[text()='"+Menu+"']"));
+//		Thread.sleep(1000);  
+		driver.findElement(By.xpath("//span[text()='"+Menu+"']")).click();
+		waitUntilPresence(By.xpath("//a[text()='"+Submenu+"']"));
+		driver.findElement(By.xpath("//a[text()='"+Submenu+"']")).click();
+	}
+	
+	public void waitUntilPresence(By by)
+	{
+//		getWebdriverWait().until(ExpectedConditions.presenceOfElementLocated(by));
+//		getWebdriverWait().until(ExpectedConditions.visibilityOf(findElement(by))); //generates StackOverfow
+//		getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(by));
+	}
+	
+	private WebDriverWait getWebdriverWait(){
+		if(wait==null)
+		{
+			wait = new WebDriverWait(driver, 30);
+			return wait;
+		}
+		else
+		{
+			return wait;
+		}
+	}
+	
+	private Wait<WebDriver> getFluentWait()
+	{
+		if(w==null){
+			w=new FluentWait<WebDriver>(driver)
+					.withTimeout(Duration.ofSeconds(30))
+					.pollingEvery(Duration.ofMillis(600))
+					.ignoring(NoSuchElementException.class);
+			return w;
+		}
+		else{
+			return w;
+		}
+		
+		
+	}
+//	public void getProperty() throws IOException
+//	{
+//		BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Jitinder Pal\\git\\repository\\Flipkart\\OR.properties"));
+//		prop=new Properties();
+//		prop.load(reader);
+//		reader.close();
+//		
+//			
+//	}
 }
